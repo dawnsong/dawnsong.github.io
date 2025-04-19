f751943c5f172ea578ba747ee2e5b4070da9850a
Xiaowei Song <dawnwei.song@gmail.com>
2025-04-19 11:49:07 -0400

    update my playlist twice a week

---
+++ hifini/cronHifini.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+source $HOME/.bashrc
+export PATH=$HOME/anaconda3/envs/webot/bin/:$HOME/anaconda3/bin:$PATH
+cd /home/dawnsong/dawn/dawnsong.github.io/hifini
+#conda activate webot
+
+exec > >(tee hifini.log) 2>&1
+python ./hifini.py sign
+
+#check if today is Sunday, if is then I will download music/songs from my online fav
+#if [[ $(date +%a) == "Sat" && "AM" == $(date +%p) ]]; then
+if [[ $(date +%a) =~ "^(Sat|Wed)$" && "AM" == $(date +%p) ]]; then
+    timeout -k 1m -s SIGKILL 10h python ./hifini.py 
+    #upload/rsync songs to google bucket
+    gcloud storage rsync   $HOME/dawn/dawnsong.github.io/hifini/songs/ gs://xmusic/q/ --recursive --delete-unmatched-destination-objects
+fi
