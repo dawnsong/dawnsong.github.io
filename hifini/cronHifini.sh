0d89e70eaf260168f0ea92fc4586f2d0e313c805
dawnsong <dawnsong.tsinghua@gmail.com>
2025-04-20 01:00:02 +0000

    Deploying to gh-pages from @ dawnsong/dawnsong.github.io@1793bbcd2efd91eda4e68bbd07d9388264f427e0 🚀

---
+++ hifini/cronHifini.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+source $HOME/.bashrc
+export PATH=$HOME/anaconda3/envs/webot/bin/:$HOME/anaconda3/bin:$PATH
+cd /home/dawnsong/dawn/dawnsong.github.io/hifini
+#conda activate webot
+
+exec > >(tee hifini.log) 2>&1
+python ./hifini.py sign
+
+#check if today is Sunday, if is then I will download music/songs from my online fav
+#if [[ $(date +%a) == "Sat" && "AM" == $(date +%p) ]]; then
+if [[ $(date +%a) =~ "^(Sat|Wed)$" && "AM" == $(date +%p) ]]; then
+    timeout -k 1m -s SIGKILL 10h python ./hifini.py 
+    ./findNonSongs.sh -rm  2>&1 |tee nonSongs.log
+    #upload/rsync songs to google bucket
+    gcloud storage rsync  ./songs/ gs://xmusic/q/ --recursive --delete-unmatched-destination-objects
+fi
