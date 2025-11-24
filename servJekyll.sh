ef2cdb83e04d154d532e06b57b354ed93a7ee63a
dawnsong <dawnsong.tsinghua@gmail.com>
2025-11-24 00:00:02 +0000

    Deploying to gh-pages from @ dawnsong/dawnsong.github.io@8ddfcca8ae0492670e2ba98cbd3b573e605612f1 🚀

--- servJekyll.sh
+++ servJekyll.sh
@@ -1,5 +1,9 @@
 #/bin/bash 
 set -x
 echo "Using MINGW64 `which -a bundle`"
-bundle exec jekyll serve  --host 0.0.0.0 #--livereload
+#bundle exec jekyll serve  --host 0.0.0.0 #--livereload
 #bundle exec jekyll serve  --incremental
+#
+#export JEKYLL_ENV=development
+#bundle exec jekyll serve  --verbose --ssl-key .ssl/xmen.key --ssl-cert .ssl/xmen.crt --host 0.0.0.0 #--livereload
+bundle exec jekyll serve  --ssl-key .ssl/xmen.key --ssl-cert .ssl/xmen.crt --host 0.0.0.0 #--livereload
